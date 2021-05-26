26ecf637e7025f698e59476f840874f90d37fa8d
CentOS <centos@centos8.linuxvmimages.local>
2021-05-26 16:00:30 +0530

    final version

--- Terraform/bucket/variable.tf
+++
@@ -1,3 +0,0 @@
-variable "bucket_name" { default = "udemy-test-bucket-gcp" }
-variable "location" { default = "europe-west1" }
-variable "storage" { default = "REGIONAL" }
--- Terraform/virtual_machine_prod_version/provider.tf
+++
@@ -1,7 +0,0 @@
-
-variable "path" {default = "/home/centos/Terraform/Credentials"}
-provider "google" {
-    project="gcp-terraform-udemy"
-    region="europe-west-a"
-    credentials = "${file("${var.path}/gcp-terraform-udemy-secrets.json")}"
-}
